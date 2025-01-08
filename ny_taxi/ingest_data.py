366589669abb9e7b7ec7d8642a4e0ba44fcfc8ad
koomited <koomi@aims.ac.za>
2025-01-08 14:10:07 +0000

    changes2

--- ny_taxi/ingest_data.py
+++ ny_taxi/ingest_data.py
@@ -24,6 +24,7 @@ def main(params):
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
     
     engine.connect()
+    print("GREAT DONE")
     
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
     
